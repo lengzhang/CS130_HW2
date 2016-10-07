6e57970ce3f2a66a3e3facedf5fc9924afc45ed6
lengzhang <lzhan021@well.cs.ucr.edu>
2016-10-07 01:11:44 -0700

    add red area

--- main.cpp
+++ main.cpp
@@ -205,8 +205,10 @@ void draw_circle(int x, int y, int r)
         data[x+X][y-Y] = 0xffffffff;
         data[x-X][y+Y] = 0xffffffff;
         data[x-X][y-Y] = 0xffffffff;
-	for (int i = x - X + 1; i < x + X - 1; i++)
+	for (int i = x - X + 1; i < x + X; i++)
 		data[i][y+Y] = 0xff0000ff;
+	for (int i = x - X + 1; i < x + X; i++)
+                data[i][y-Y] = 0xff0000ff;
     }
 
     Dump_png(&data[0][0],512,512,"circle.png");
